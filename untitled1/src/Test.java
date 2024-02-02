fffacf202f5a59c0ae6da1493d5b045f4f169c35
sunwei6-jk <sunwei6-jk@360shuke.com>
2024-02-02 11:34:23 +0800

    Revert "测试"
    This reverts commit 71b23c70f4059eab73b35adcdb039bcaae9bfab5.

--- untitled1/src/Test.java
+++ untitled1/src/Test.java
@@ -8,5 +8,8 @@ public class Test {
     public static void main(String[] args) {
         Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();//two4
         cnt.getOrDefault(1,0);
+        List list = new ArrayList<>();
+//        list.stream().filter()
+
     }
 }
